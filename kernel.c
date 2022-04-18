81764a9fad1beba53418c9fba090a2240f242a9b
DevNugget <84568105+DevNugget@users.noreply.github.com>
2022-04-18 21:22:54 +0530

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -1,6 +1,23 @@
+char *vidmem = (char *)0xb8000;
+
+void print(char* string, int color) {
+	int chars;
+	int charsInStr = 0;
+
+	for (chars = 0; string[chars] != '\0'; chars++);
+
+	for (int j = 0; j < chars + 1; j++) {
+		j += 1;
+		vidmem[j] = color;
+	}
+	for (int i = 0; i < chars*2; i++) {
+		vidmem[i] = string[charsInStr];
+		i += 1;
+		charsInStr += 1;
+	}
+}
+
 int kmain()
 {
-	char *vidmem = (char *)0xb8000;
-	vidmem[0] = 'A';
-	vidmem[1] = 0x07;
+	print("Hello World!", 0x07);
 }
